64e6695bd832f2b1ca717c7d7590249a2409363b
Jeremy Manin <Jmm350@pitt.edu>
2018-04-12 18:46:04 -0400

    Orientation offsets (#5)
    * Added orientation offsets
    
    * Fixed reading packet
    
    * Fixed documentation

--- Dock/CommBridge.c
+++ Dock/CommBridge.c
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+static float X_OFFSET = 0;
+static float Y_OFFSET = 0;
+static float Z_OFFSET = 0;
+
 static int32_t start_buf = 0;
 static FILE *f_ptr = NULL;
 
@@ -19,37 +23,57 @@ int COMM_monitor() {
     
     ioctl(fileno(f_ptr), FIONREAD, &nread);
     
-    if(nread > 0)
-        return(0);
-    else
-        return(1);
+	return(nread);
 }
 
 int COMM_bridgeInit() {
+	serial_packet_t temp;
+	int packet_count = 0;
+	float x_sum = 0, y_sum = 0, z_sum = 0;
+
 	f_ptr = fopen(PORT, "r");
 	
 	if(f_ptr==NULL)
 		return(1);
 	else
+	{
+		while(packet_count<5)
+		{
+			temp = COMM_getNextPacket();
+			
+			x_sum += temp.serial_orientation[0];
+			y_sum += temp.serial_orientation[1];
+			z_sum += temp.serial_orientation[2];								
+			
+			packet_count++;
+		}
+		
+		X_OFFSET = x_sum/packet_count;
+		Y_OFFSET = y_sum/packet_count;
+		Z_OFFSET = z_sum/packet_count;
+		
 		return(0);
+	}
 }
 
 serial_packet_t COMM_getNextPacket() {
     serial_packet_t packet_to_recv;
 	uint8_t byte_buf = 0;
 	uint32_t temp = 0;
-    
-    if(start_buf == START_VAL)
-    {
-		fread(&packet_to_recv, sizeof(struct serial_packet), 1, f_ptr);
-
-		return(packet_to_recv);
-    }
-    else
-    {
+	
+	while(start_buf != START_VAL)
+	{
       fread(&byte_buf, sizeof(uint8_t), 1, f_ptr);
       start_buf = (uint32_t)start_buf >> 8;
       temp = (uint32_t)byte_buf << 24;
-      start_buf = start_buf | temp;
-    }
+      start_buf = start_buf | temp;	
+	}
+
+	fread(&packet_to_recv, sizeof(struct serial_packet), 1, f_ptr);
+
+	packet_to_recv.serial_orientation[0] -= X_OFFSET;
+	packet_to_recv.serial_orientation[1] -= Y_OFFSET;
+	packet_to_recv.serial_orientation[2] -= Z_OFFSET;		
+
+	return(packet_to_recv);
 }
